474d180b2f2ecc6662f144f1b984b24110ae4109
Salvatore <salvatore.a1995@hotmail.fr>
2026-06-17 20:57:46 +0200

    fix screen effects to exclude itself

--- Views/ScreenEffects/EffectOverlayWindow.axaml.cs
+++ Views/ScreenEffects/EffectOverlayWindow.axaml.cs
@@ -7,13 +7,15 @@ namespace CrowdKeys.Views;
 
 public partial class EffectOverlayWindow : Window
 {
-    [DllImport("user32.dll")] private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-    [DllImport("user32.dll")] private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+    [DllImport("user32.dll")] private static extern int  GetWindowLong(IntPtr hWnd, int nIndex);
+    [DllImport("user32.dll")] private static extern int  SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+    [DllImport("user32.dll")] private static extern bool SetWindowDisplayAffinity(IntPtr hWnd, uint dwAffinity);
 
-    private const int GWL_EXSTYLE      = -20;
-    private const int WS_EX_LAYERED    = 0x00080000;
-    private const int WS_EX_TRANSPARENT = 0x00000020;
-    private const int WS_EX_NOACTIVATE = 0x08000000;
+    private const int  GWL_EXSTYLE             = -20;
+    private const int  WS_EX_LAYERED           = 0x00080000;
+    private const int  WS_EX_TRANSPARENT       = 0x00000020;
+    private const int  WS_EX_NOACTIVATE        = 0x08000000;
+    private const uint WDA_EXCLUDEFROMCAPTURE  = 0x00000011;
 
     public EffectOverlayWindow()
     {
@@ -25,12 +27,19 @@ public partial class EffectOverlayWindow : Window
 
     private void OnOpened(object? sender, EventArgs e)
     {
-        if (TryGetPlatformHandle() is { } handle)
-        {
-            var exStyle = GetWindowLong(handle.Handle, GWL_EXSTYLE);
-            SetWindowLong(handle.Handle, GWL_EXSTYLE,
-                exStyle | WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_NOACTIVATE);
-        }
+        if (TryGetPlatformHandle() is not { } handle)
+            return;
+
+        var hwnd = handle.Handle;
+
+        // Click-through + no focus steal
+        var exStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+        SetWindowLong(hwnd, GWL_EXSTYLE,
+            exStyle | WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_NOACTIVATE);
+
+        // Exclude this window from GDI/DX screen captures so the live
+        // capture loop doesn't capture the overlay itself (feedback loop).
+        SetWindowDisplayAffinity(hwnd, WDA_EXCLUDEFROMCAPTURE);
     }
 
     public void StartEffect(IScreenEffect effect, SKBitmap frame) =>
