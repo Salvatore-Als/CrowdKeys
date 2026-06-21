48bd9cde2bd6ad048dea5ef6824c278323dbade4
Salvatore <salvatore.a1995@hotmail.fr>
2026-06-21 13:26:15 +0200

    Preview title

--- Views/ScreenEffects/PreviewWindow.axaml.cs
+++ Views/ScreenEffects/PreviewWindow.axaml.cs
@@ -1,5 +1,3 @@
-using System.Runtime.InteropServices;
-using Avalonia;
 using Avalonia.Controls;
 using CrowdKeys.ScreenEffects.Effects;
 using SkiaSharp;
@@ -8,27 +6,9 @@ namespace CrowdKeys.Views;
 
 public partial class PreviewWindow : Window
 {
-    [DllImport("user32.dll")] private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-    [DllImport("user32.dll")] private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-
-    private const int GWL_EXSTYLE      = -20;
-    private const int WS_EX_TOOLWINDOW = 0x00000080; // hide from Alt+Tab
-
     public PreviewWindow()
     {
         InitializeComponent();
-        Position = new PixelPoint(-32000, -32000);
-
-        if (OperatingSystem.IsWindows())
-            Opened += OnOpened;
-    }
-
-    private void OnOpened(object? sender, EventArgs e)
-    {
-        if (TryGetPlatformHandle() is not { } handle)
-            return;
-        var exStyle = GetWindowLong(handle.Handle, GWL_EXSTYLE);
-        SetWindowLong(handle.Handle, GWL_EXSTYLE, exStyle | WS_EX_TOOLWINDOW);
     }
 
     public void StartEffect(IScreenEffect effect, SKBitmap frame) =>
